d2cd56812092fd8b686e6e6823c20961e23a1375
Jani Suihko <janigbg@gmail.com>
2018-11-25 14:06:44 +0100

    Make Board component stateless

--- src/index.js
+++ src/index.js
@@ -139,42 +139,39 @@ const Cell = ({index, value, isClue, onChange}) => {
     );
 }
 
-// <input id={(row*9+col).toString()} type="text" value={board[row*9+col] > 0 ? board[row*9+col] : ' '} disabled={board[row*9+col] > 0}></input>
-class Board extends Component {
-    render () {
-        if (this.props.board == null || this.props.board.len == 0) return (<div></div>);
+const Board = ({board, clues, handleChange}) => {
+    if (board == null || board.len == 0) return (<div></div>);
 
-        let table = [];
+    let table = [];
 
-        // Outer loop to create parent
-        for (let row = 0; row < 9; row++) {
-            let children = [];
-            //Inner loop to create children
-            for (let col = 0; col < 9; col++) {
-                let cell = this.props.board[row*9+col];
-                let isClue = this.props.clues[row*9+col];
-                children.push(
-                    <Cell
-                        key={"cell_"+(row*9+col)}
-                        index={(row*9+col).toString()}
-                        value={cell > 0 ? cell : ''}
-                        isClue={isClue}
-                        onChange={this.props.handleChange}  />);
-            }
-            //Create the parent and add the children
-            table.push(<StyledTr key={"row_"+row}>{children}</StyledTr>);
+    // Outer loop to create parent
+    for (let row = 0; row < 9; row++) {
+        let children = [];
+        //Inner loop to create children
+        for (let col = 0; col < 9; col++) {
+            let cell = board[row*9+col];
+            let isClue = clues[row*9+col];
+            children.push(
+                <Cell
+                    key={"cell_"+(row*9+col)}
+                    index={(row*9+col).toString()}
+                    value={cell > 0 ? cell : ''}
+                    isClue={isClue}
+                    onChange={handleChange}  />);
         }
-
-        return(
-            <StyledBlock>
-                <StyledTable>
-                    <StyledTbody>
-                        {table}
-                    </StyledTbody>
-                </StyledTable>
-            </StyledBlock>
-        );
+        //Create the parent and add the children
+        table.push(<StyledTr key={"row_"+row}>{children}</StyledTr>);
     }
+
+    return(
+        <StyledBlock>
+            <StyledTable>
+                <StyledTbody>
+                    {table}
+                </StyledTbody>
+            </StyledTable>
+        </StyledBlock>
+    );
 }
 
 class Puzzle extends Component {
